585efecb44206433bfe25fd9e1bf959f7b9158d9
Ada Lovelace <mila.nik.91@yandex.ru>
2022-01-27 02:57:48 +0300

    last version notifications done

--- quasar.conf.js
+++ quasar.conf.js
@@ -24,7 +24,7 @@ module.exports = configure(function (ctx) {
     // --> boot files are part of "main.js"
     // https://quasar.dev/quasar-cli/boot-files
     boot: [
-      'axios', 'firebase', 'routes-guard', 'calendarPlugin', 'apexcharts'
+      'axios', 'firebase', 'routes-guard', 'calendarPlugin',
     ],
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
